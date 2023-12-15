3d93cf19df49965f25974aeecfb2e5f5ad439300
100yann <stoyan.d.kolev@gmail.com>
2023-12-15 12:21:50 +0200

    on book entry click display only that book, on another click display all results again

--- static/search_form.js
+++ static/search_form.js
@@ -1,39 +1,71 @@
 window.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('search-form')
     
-    form.onsubmit = async (event) =>{
-        const searchValue = document.getElementById('search').value
+    form.onsubmit = async (event) => {
         event.preventDefault()
+        const searchValue = document.getElementById('search').value
 
+        // Check if there is any search input
         if (searchValue){
-            const response = await fetch(`/find?search=${searchValue}`)
-            const data = await response.json()
-            if (data){
-                const resultsTab = document.getElementById('results-tab')
-                resultsTab.innerHTML = ''                
-                resultsTab.style.display = 'block'
+            // Display the results on the page
+            await displaySearchResults(searchValue)
 
-                data.forEach(element => {
-                    if ('imageLinks' in element.volumeInfo){
-                        let div = document.createElement('div')
-                        div.innerHTML = `
-                        <div id='displayEntry'>
-                            <div id='entryThumbnail'>
-                                <img src=${element.volumeInfo.imageLinks.thumbnail}>
-                            </div>
-                            <div id='entryDetails'>
-                                <h2>${element.volumeInfo.title}</h2>
-                                <h3>${element.volumeInfo.authors}</h3>
-                                <p>${element.volumeInfo.description}</p>
-                                <p>${element.volumeInfo.pageCount}</p>
-                            </div
-                        </div>
-                            `
-                        resultsTab.append(div)                        
+            const resultEntries = document.getElementsByClassName('displayEntry')
+            const resultArray = Array.from(resultEntries)
+            resultArray.forEach((element1) => {
+                // On click of a book entry hide all the other entries
+                // and display only the clicked entry
+                element1.onclick = () => {
+                    resultArray.forEach((element2) => {
+                        if (element1 != element2){
+                            element2.style.display = 'none'
+                        }
+                    })
+                    // If currently only one entry is displayed and it's
+                    // clicked again - show all entries again
+                    if (element1.style.display === 'block') {
+                        resultArray.forEach((element2) => {
+                            element2.style.display = 'flex';
+                        });
+                    } else {
+                        element1.style.display = 'block';
                     }
-
-                });
-            }   
+                }
+            })
         }
     }
 })
+
+// Display books returned by the Google API
+async function displaySearchResults(searchValue){
+    // fetch the results
+    const response = await fetch(`/find?search=${searchValue}`)
+    const data = await response.json()
+
+    if (data){
+        // display the results tab where all results will be displayed
+        const resultsTab = document.getElementById('results-tab')
+        resultsTab.innerHTML = ''                
+        resultsTab.style.display = 'block'
+
+        data.forEach(element => {
+            // display only results that have cover photos
+            if ('imageLinks' in element.volumeInfo){
+                let div = document.createElement('div')
+                div.className = 'displayEntry'
+                div.innerHTML = `
+                    <div id='entryThumbnail'>
+                        <img src=${element.volumeInfo.imageLinks.thumbnail}>
+                    </div>
+                    <div id='entryDetails'>
+                        <h2>${element.volumeInfo.title}</h2>
+                        <h3>${element.volumeInfo.authors}</h3>
+                        <p>${element.volumeInfo.description}</p>
+                        <p>${element.volumeInfo.pageCount}</p>
+                    </div
+                    `
+                resultsTab.append(div)                        
+            }
+        });
+    }   
+}
