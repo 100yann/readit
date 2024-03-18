52f4e3798005fe2c4100b6ca6359abd5e30de428
100yann <stoyan.d.kolev@gmail.com>
2024-03-18 18:39:10 +0200

    create a new branch to rewrite my fastapi code

---
+++ fastapi/routers/posts.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix='/posts',
+    tags=['Posts']
+)
+
+
+# Get all posts
+
+
+# Create a post
+
+
+# Delete a post
+
+
+# Update a post
+
+
+# Get post by ID 
---
+++ fastapi/routers/users.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix='/users',
+    tags=['Users']
+)
+
+
+# Create a user
+
+
+# Get user by id
