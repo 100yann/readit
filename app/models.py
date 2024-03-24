5489cd866494f80929c1c494c36bd33965e664c2
100yann <stoyan.d.kolev@gmail.com>
2024-03-24 19:13:07 +0200

    change postgres password column to binary type

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, LargeBinary
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -8,7 +8,7 @@ class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    password = Column(LargeBinary, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False,
                         server_default=text('now()')
